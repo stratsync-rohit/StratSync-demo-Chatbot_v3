6ac6043a46f5bcbb5f7fdd7fe04f6b6d868a5a42
stratsync-rohit <rohit@stratsync.ai>
2025-11-18 11:36:59 +0530

    update

--- client/src/components/ChatWindow.tsx
+++ client/src/components/ChatWindow.tsx
@@ -355,7 +355,7 @@ const ChatWindow: React.FC = () => {
             nonJsonCanSummarize = false;
           }
         } catch (e) {
-          /* ignore */
+          /* ignore  this error*/ 
         }
 
         const assistantMessage: Message = {
